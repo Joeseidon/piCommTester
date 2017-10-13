d35438e1f35de6ff5930ceead589f1f30af0c77b
Joseph Cutino <joseph.cutino@psware.com>
2017-10-13 12:14:07 -0400

    working out ideas

--- OptionsPane.py
+++ OptionsPane.py
@@ -7,3 +7,5 @@ class OptionsPane(wx.Panel):
         self.eventLoglbl = wx.StaticText(self, label = "Event Logs:",pos=(200,600))
 
         self.eventLogger = wx.TextCtrl(self, pos=(200,625), size=(400,125), style = wx.TE_MULTILINE | wx.TE_READONLY)
+
+        
--- spi_com.py
+++ spi_com.py
@@ -1,10 +1,10 @@
 
 class SPI():
     #Raspberry Pi SPI pins
-    CLK =
-    MISO =
-    MOSI =
-    CS =
+    SCLK = 23
+    MISO = 21
+    MOSI = 19
+    CE0 = 24
+    CE1 = 26
 
     def __init__(self)
-        
